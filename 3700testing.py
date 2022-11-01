7aceb1e2bb363864783402146c0cff5373e11d31
justagoodfriend <greenate1@gmail.com>
2022-11-01 13:19:57 -0400

    level 2 test passed

--- 3700testing.py
+++ 3700testing.py
@@ -1,6 +1,13 @@
 import re
-String1 = "----- Block 0000011 ------- dsakjlfadjkfldajflkadj"
-pattern = re.compile(r'[0][0][0][0][0]\d\d')
-match = re.findall(pattern, String1)
-list1 = ["0000010", "00000001"]
-print(match in list1)
+String1 = "----- Block 0000011 ----- dsakjlfadjkfldajflkadj"
+substring = String1[13:19]
+print(substring)
+
+testdict = {"type": "ack", "sequence" : "7"}
+print(testdict["sequence"])
+
+#pattern = re.compile(r'[0][0][0][0][0]\d\d')
+#match = re.findall(pattern, String1)
+#list1 = ["0000010", "00000001"]
+
+#print(match in list1)
